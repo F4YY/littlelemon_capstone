fad12e7a7afb081934a6a1693a4e4fb77cb7a6a4
riki wendri <rikiwendri@gmail.com>
2022-12-21 12:42:37 +0700

    simplify code app

---
+++ little_lemon/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          I am Software Engineer
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
